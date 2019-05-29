561d30c8ae0f21873a9f1e435987610ab677db13
szwei <854989215@qq.com>
2019-05-29 16:35:10 +0800

    eureka集群搭建

---
+++ microservicecloud-eureka-7002/src/main/java/cn/couldme/springcloud/EurekaServer7002_App.java
@@ -0,0 +1,18 @@
+package cn.couldme.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Author: szwei
+ * @date : 2019/5/28 10:57
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServer7002_App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer7002_App.class, args);
+    }
+}
