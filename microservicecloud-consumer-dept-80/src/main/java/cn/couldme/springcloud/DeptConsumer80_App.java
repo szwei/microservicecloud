7125e2053033d67fb224a9ff8026507276119de8
szwei <854989215@qq.com>
2019-05-30 15:43:23 +0800

    Ribbon配置初步

--- microservicecloud-consumer-dept-80/src/main/java/cn/couldme/springcloud/DeptConsumer80_App.java
+++ microservicecloud-consumer-dept-80/src/main/java/cn/couldme/springcloud/DeptConsumer80_App.java
@@ -2,12 +2,14 @@ package cn.couldme.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @Author: szwei
  * @date : 2019/5/27 21:00
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class DeptConsumer80_App {
 
     public static void main(String[] args) {
