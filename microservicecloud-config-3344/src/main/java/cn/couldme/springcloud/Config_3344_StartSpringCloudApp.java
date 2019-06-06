4fe7d3691228fbb4fccfe44556946f29cf9b7f0d
szwei <854989215@qq.com>
2019-06-06 09:42:21 +0800

    Config server 配置

---
+++ microservicecloud-config-3344/src/main/java/cn/couldme/springcloud/Config_3344_StartSpringCloudApp.java
@@ -0,0 +1,17 @@
+package cn.couldme.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author: szwei
+ * @Date: 2019-06-05 21:13
+ **/
+@SpringBootApplication
+@EnableConfigServer
+public class Config_3344_StartSpringCloudApp {
+    public static void main(String[] args) {
+        SpringApplication.run(Config_3344_StartSpringCloudApp.class, args);
+    }
+}
