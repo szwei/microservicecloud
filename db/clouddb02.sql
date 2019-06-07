b861e1d9f005f6e42216d6054f3bda5bcc7dc487
szwei <854989215@qq.com>
2019-06-07 18:46:00 +0800

    添加数据库文件

---
+++ db/clouddb02.sql
@@ -0,0 +1,40 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost
+ Source Server Type    : MySQL
+ Source Server Version : 50644
+ Source Host           : localhost:3306
+ Source Schema         : clouddb02
+
+ Target Server Type    : MySQL
+ Target Server Version : 50644
+ File Encoding         : 65001
+
+ Date: 07/06/2019 18:44:40
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for dept
+-- ----------------------------
+DROP TABLE IF EXISTS `dept`;
+CREATE TABLE `dept`  (
+  `deptno` bigint(20) NOT NULL AUTO_INCREMENT,
+  `dname` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `db_source` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`deptno`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Records of dept
+-- ----------------------------
+INSERT INTO `dept` VALUES (1, '开发部', 'clouddb02');
+INSERT INTO `dept` VALUES (2, '人事部', 'clouddb02');
+INSERT INTO `dept` VALUES (3, '财务部', 'clouddb02');
+INSERT INTO `dept` VALUES (4, '市场部', 'clouddb02');
+INSERT INTO `dept` VALUES (5, '运维部', 'clouddb02');
+
+SET FOREIGN_KEY_CHECKS = 1;
